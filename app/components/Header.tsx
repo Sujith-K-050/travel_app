431c0ab40558da362e29afd89eb8ef1e065376e5
Sujith-K-050 <sujithyadav050@gmail.com>
2025-08-29 04:00:42 +0530

    Dashboard Components - TripCard, StatsCard Implemented

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,27 +9,29 @@ interface HeaderProps {
 const Header = ({ title, description }: HeaderProps) => {
   const location = useLocation();
   return (
-    <header className="header">
-      <h1
-        className={cn(
-          "text-dark-100",
-          location.pathname === "/"
-            ? "text-2xl md:text-4xl font-bold"
-            : "text-xl md:text-2xl font-bold"
-        )}
-      >
-        {title}
-      </h1>
-      <p
-        className={cn(
-          "text-gray-100 font-normal",
-          location.pathname === "/"
-            ? "text-base md:text-lg "
-            : "text-sm md:text-lg "
-        )}
-      >
-        {description}
-      </p>
+    <header className="header mt-8">
+      <div className="flex flex-col justify-left">
+        <h1
+          className={cn(
+            "text-dark-100",
+            location.pathname === "/"
+              ? "text-2xl md:text-4xl font-bold"
+              : "text-xl md:text-2xl font-bold"
+          )}
+        >
+          {title}
+        </h1>
+        <p
+          className={cn(
+            "text-gray-100 font-normal",
+            location.pathname === "/"
+              ? "text-base md:text-lg "
+              : "text-sm md:text-lg "
+          )}
+        >
+          {description}
+        </p>
+      </div>
     </header>
   );
 };
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,16 +1,56 @@
-import { Header } from "~/components";
+import { Header, StatsCard, TripCard } from "~/components";
+import { DashboardStats, user, allTrips } from "~/constants";
+
+const { totalUser, usersJoined, totalTrips, tripsCreated, userRole } =
+  DashboardStats;
 
 const Dashboard = () => {
-  const user = {
-    name: "Sujith",
-  };
   return (
     <main className="dashboard wrapper">
       <Header
         title={`Welcome ${user?.name ? user.name : "Guest"} 👋`}
         description="Track activity, trends and popular destinations in real time."
       />
-      Dashboard Page Content
+      <section className="flex flex-col gap-8">
+        <div className="grid  grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalUser}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className="text-xl text-semibold text-dark-100">Created Trips</h1>
+        <div className="trip-grid">
+          {allTrips
+            .slice(0, 4)
+            .map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+              <TripCard
+                key={id}
+                id={id.toString()}
+                name={name}
+                imageUrl={imageUrls[0]}
+                location={itinerary?.[0]?.location ?? ""}
+                tags={tags}
+                price={estimatedPrice}
+              />
+            ))}
+        </div>
+      </section>
     </main>
   );
 };
